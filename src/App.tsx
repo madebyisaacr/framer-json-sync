53c547973a7784316c4e7f64f3234f07aa1357ab
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-01-12 09:04:22 -0500

    Remove theme change code

--- src/App.tsx
+++ src/App.tsx
@@ -57,21 +57,8 @@ export function App({ collection }: { collection: Collection | null }) {
     useEffect(() => {
         if (selectedCollection) {
             selectedCollection.setAsActive()
-
-            if (!initialCollection) {
-                const body = document.body
-                for (let i = 0; i < body.style.length; i++) {
-                    if (
-                        body.style[i] === "--framer-color-bg" &&
-                        body.style.getPropertyValue("--framer-color-bg") === "#111111"
-                    ) {
-                        body.style.setProperty("--framer-color-bg", "#181818")
-                        break
-                    }
-                }
-            }
         }
-    }, [selectedCollection, initialCollection])
+    }, [selectedCollection])
 
     const importItems = useCallback(
         async (result: ImportResult) => {
